7fec302ee1d809d2fee29093287c875cfa2c330e
Dido <dido_nikolov@abv.bg>
2024-03-07 07:03:04 +0200

    Add additional validations when creating workout

--- src/main/java/com/softuni/fitlaunch/model/dto/workout/CreateWorkoutDTO.java
+++ src/main/java/com/softuni/fitlaunch/model/dto/workout/CreateWorkoutDTO.java
@@ -2,28 +2,35 @@ package com.softuni.fitlaunch.model.dto.workout;
 
 import com.softuni.fitlaunch.model.dto.ExerciseDTO;
 import com.softuni.fitlaunch.model.enums.LevelEnum;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CreateWorkoutDTO {
 
-    private Long id;
-
-    @NotEmpty
+    @NotBlank(message = "Name should not be null nor empty")
+    @Length(max = 255, message = "Name cannot be more than 255 characters long")
+    @Pattern(regexp = "^(?!\\s)[a-zA-Z0-9-* ]+", message = "Invalid workout name")
     private String name;
 
     @NotNull
     private LevelEnum level;
 
-    @NotEmpty @Size(min = 5, max = 512)
+    @NotBlank(message = "Description should not be null nor empty")
+    @Length(max = 255, message = "Description cannot be more than 255 characters long")
     private String description;
 
-
+    @NotNull(message = "Image should not be null")
     private MultipartFile imgUrl;
 
     @NotNull
@@ -33,102 +40,4 @@ public class CreateWorkoutDTO {
 
     private List<Integer> sets;
     private List<Integer> reps;
-
-
-
-    public CreateWorkoutDTO() {
-    }
-
-    public CreateWorkoutDTO(Long id, String name, LevelEnum level, String description, MultipartFile imgUrl, List<ExerciseDTO> exercises, List<Long> selectedExerciseIds, List<Integer> sets, List<Integer> reps) {
-        this.id = id;
-        this.name = name;
-        this.level = level;
-        this.description = description;
-        this.imgUrl = imgUrl;
-        this.exercises = exercises;
-        this.selectedExerciseIds = selectedExerciseIds;
-        this.sets = sets;
-        this.reps = reps;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public CreateWorkoutDTO setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public CreateWorkoutDTO setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public LevelEnum getLevel() {
-        return level;
-    }
-
-    public CreateWorkoutDTO setLevel(LevelEnum level) {
-        this.level = level;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public CreateWorkoutDTO setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public MultipartFile getImgUrl() {
-        return imgUrl;
-    }
-
-    public CreateWorkoutDTO setImgUrl(MultipartFile imgUrl) {
-        this.imgUrl = imgUrl;
-        return this;
-    }
-
-    public List<ExerciseDTO> getExercises() {
-        return exercises;
-    }
-
-    public CreateWorkoutDTO setExercises(List<ExerciseDTO> exercises) {
-        this.exercises = exercises;
-        return this;
-    }
-
-    public List<Long> getSelectedExerciseIds() {
-        return selectedExerciseIds;
-    }
-
-    public CreateWorkoutDTO setSelectedExerciseIds(List<Long> selectedExerciseIds) {
-        this.selectedExerciseIds = selectedExerciseIds;
-        return this;
-    }
-
-    public List<Integer> getSets() {
-        return sets;
-    }
-
-    public CreateWorkoutDTO setSets(List<Integer> sets) {
-        this.sets = sets;
-        return this;
-    }
-
-    public List<Integer> getReps() {
-        return reps;
-    }
-
-    public CreateWorkoutDTO setReps(List<Integer> reps) {
-        this.reps = reps;
-        return this;
-    }
 }
