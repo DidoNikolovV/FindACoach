cb23ed63b7f4fbf0bac0841a2e9733911e87723b
Dido <dido_nikolov01@abv.bg>
2024-03-21 22:31:32 +0200

    Delete UserProfileService and UserProfileRepository.

--- src/main/java/com/softuni/fitlaunch/repository/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softuni.fitlaunch.repository;
-
-import com.softuni.fitlaunch.model.entity.UserProfileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
-}
