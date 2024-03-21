62c7546051132ca605b6d4664b8e14a6ad75e7e2
Dido <dido_nikolov01@abv.bg>
2024-03-21 19:47:34 +0200

    Fix details page for client

--- src/main/java/com/softuni/fitlaunch/web/ClientController.java
+++ src/main/java/com/softuni/fitlaunch/web/ClientController.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.List;
 
 @Controller
+@RequestMapping("/clients")
 public class ClientController {
 
     private final ClientService clientService;
@@ -26,7 +28,7 @@ public class ClientController {
     }
 
 
-    @GetMapping("/clients/{coach}/all")
+    @GetMapping("/{coach}/all")
     public String loadAllClientsForCoach(@PathVariable("coach") String username, Model model) {
         CoachDTO coach = coachService.getCoachByUsername(username);
         List<ClientDTO> clients = clientService.loadAllByCoach(coach);
@@ -34,5 +36,15 @@ public class ClientController {
         model.addAttribute("clients", clients);
 
         return "clients";
+    }
+
+    @GetMapping("/{clientName}/details")
+    public String loadClientDetails(@PathVariable("clientName") String clientName, Model model) {
+        ClientDTO client = clientService.getClientByUsername(clientName);
+
+        model.addAttribute("client", client);
+
+        return "client-details";
+
     }
 }
