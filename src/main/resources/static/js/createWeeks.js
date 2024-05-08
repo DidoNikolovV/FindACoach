1d12413b143a8d3f4163815a88ed8eec0665b8c9
Dido <dido_nikolov01@abv.bg>
2024-05-08 20:49:39 +0300

    Add modal in program-add-workouts template

---
+++ src/main/resources/static/js/createWeeks.js
@@ -0,0 +1,5 @@
+const url = 'http://localhost:8080'
+
+const csrfHeaderName = document.head.querySelector('[name=_csrf_header]').content
+const csrfHeaderValue = document.head.querySelector('[name=_csrf]').content
+
