2696f98f8057d9a9de4f4d41cc59323f9552c37b
Dido <dido_nikolov01@abv.bg>
2024-06-07 07:19:17 +0300

    Add helper form inside modal, after clicking that I need help with choosing a program

---
+++ src/main/java/com/softuni/fitlaunch/web/rest/ProgramHelperRestController.java
@@ -0,0 +1,17 @@
+//package com.softuni.fitlaunch.web.rest;
+//
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1/program-helper")
+//public class ProgramHelperRestController {
+//
+//    @GetMapping
+//    public ResponseEntity<> loadHelper() {
+//        return ResponseEntity.ok();
+//    }
+//}
