5ca4aa2ba5c094265001a20b51309fcb1b69e1a0
Dido <dido_nikolov01@abv.bg>
2024-08-14 07:19:12 +0300

    Add unit tests for CommentService

---
+++ src/test/java/com/softuni/fitlaunch/service/CommentServiceTest.java
@@ -0,0 +1,189 @@
+package com.softuni.fitlaunch.service;
+
+
+import com.softuni.fitlaunch.model.dto.comment.CommentCreationDTO;
+import com.softuni.fitlaunch.model.dto.user.UserDTO;
+import com.softuni.fitlaunch.model.dto.user.UserRoleDTO;
+import com.softuni.fitlaunch.model.dto.view.CommentView;
+import com.softuni.fitlaunch.model.entity.CommentEntity;
+import com.softuni.fitlaunch.model.entity.TopicEntity;
+import com.softuni.fitlaunch.model.entity.UserEntity;
+import com.softuni.fitlaunch.model.entity.WorkoutEntity;
+import com.softuni.fitlaunch.model.enums.UserRoleEnum;
+import com.softuni.fitlaunch.repository.CommentRepository;
+import com.softuni.fitlaunch.repository.TopicRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class CommentServiceTest {
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private WorkoutService workoutService;
+    @Mock
+    private TopicService topicService;
+    @Mock
+    private TopicRepository topicRepository;
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private CommentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testGetAllCommentsForWorkout_whenWorkoutHasComments_thenReturnAll() {
+        UserEntity user = new UserEntity();
+        user.setId(1L);
+        user.setUsername("test");
+
+        WorkoutEntity workout = new WorkoutEntity();
+        workout.setId(1L);
+        workout.setName("Full Body");
+
+        CommentEntity comment = new CommentEntity();
+        comment.setAuthor(user);
+        comment.setWorkout(workout);
+        comment.setMessage("Test comment");
+        comment.setId(1L);
+
+        when(commentRepository.findAllByWorkoutId(1L)).thenReturn(List.of(comment));
+
+        List<CommentView> allCommentsForWorkout = underTest.getAllCommentsForWorkout(1L);
+
+        assertThat(allCommentsForWorkout).isNotEmpty();
+
+        verify(commentRepository, times(1)).findAllByWorkoutId(1L);
+    }
+
+    @Test
+    void testGetComment_whenCommentInDatabase_thenReturnIt() {
+        UserEntity user = new UserEntity();
+        user.setId(1L);
+        user.setUsername("test");
+
+        WorkoutEntity workout = new WorkoutEntity();
+        workout.setId(1L);
+        workout.setName("Full Body");
+
+        CommentEntity comment = new CommentEntity();
+        comment.setAuthor(user);
+        comment.setWorkout(workout);
+        comment.setMessage("Test comment");
+        comment.setId(1L);
+
+
+        when(commentRepository.findById(1L)).thenReturn(Optional.of(comment));
+
+        CommentView result = underTest.getComment(1L);
+
+        verify(commentRepository, times(1)).findById(1L);
+
+        assertNotNull(result);
+    }
+
+    @Test
+    void testAddTopicComment_whenUserTriesToLeaveAComment_thenCreateNewCommentAndSaveIt() {
+        UserEntity user = new UserEntity();
+        user.setId(1L);
+        user.setUsername("test");
+        CommentEntity comment = new CommentEntity();
+        comment.setAuthor(user);
+        comment.setMessage("Test comment");
+        comment.setId(1L);
+
+        TopicEntity topic = new TopicEntity();
+        topic.setAuthor(user.getUsername());
+        topic.setTitle("test title");
+        topic.setContent("test content");
+        topic.setComments(new ArrayList<>());
+
+        CommentCreationDTO commentCreationDTO = new CommentCreationDTO();
+        comment.setId(1l);
+        comment.setAuthor(user);
+        comment.setMessage("test content");
+        comment.setTopic(topic);
+
+        when(userService.getUserEntityByUsername("test")).thenReturn(user);
+        when(topicService.getEntityById(1L)).thenReturn(topic);
+        when(modelMapper.map(commentCreationDTO, CommentEntity.class)).thenReturn(comment);
+
+
+
+        underTest.addTopicComment(commentCreationDTO, "username", 1L);
+
+        verify(commentRepository, times(1)).save(comment);
+    }
+
+    @Test
+    void testDeleteCommentById_whenUserIsAuthorOfTheComment_thenDeleteIt() {
+        UserDTO userDto = new UserDTO();
+        userDto.setUsername("test");
+
+        UserEntity user = new UserEntity();
+        user.setId(1L);
+        user.setUsername("test");
+
+        UserRoleDTO userRoleDto = new UserRoleDTO();
+        userRoleDto.setRole(UserRoleEnum.COACH);
+        userDto.setRoles(List.of(userRoleDto));
+        CommentEntity comment = new CommentEntity();
+        comment.setAuthor(user);
+        comment.setMessage("Test comment");
+        comment.setId(1L);
+
+        when(userService.getUserByUsername("test")).thenReturn(userDto);
+        when(commentRepository.findById(1L)).thenReturn(Optional.of(comment));
+
+        underTest.deleteCommentById(1L, "test");
+
+        verify(commentRepository, times(1)).delete(comment);
+    }
+
+    @Test
+    void testAddComment_whenUserAddComment_thenSaveIt() {
+        UserEntity user = new UserEntity();
+        user.setId(1L);
+        user.setUsername("test");
+
+        WorkoutEntity workout = new WorkoutEntity();
+        workout.setId(1L);
+        workout.setName("Full Body");
+
+        CommentCreationDTO commentCreationDTO = new CommentCreationDTO();
+
+        CommentEntity comment = new CommentEntity();
+        comment.setAuthor(user);
+        comment.setMessage("Test comment");
+        comment.setId(1L);
+
+        when(userService.getUserEntityByUsername("test")).thenReturn(user);
+        when(workoutService.getWorkoutEntityById(1L)).thenReturn(workout);
+        when(modelMapper.map(commentCreationDTO, CommentEntity.class)).thenReturn(comment);
+        when(commentRepository.save(comment)).thenReturn(comment);
+
+        underTest.addComment(commentCreationDTO, "test", 1L);
+
+        verify(commentRepository, times(1)).save(comment);
+    }
+}
